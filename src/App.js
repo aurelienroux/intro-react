23f7a93125f3489d5989461eb09322c06af94fe2
Aurelien Roux <aureroux13@me.com>
2017-02-14 21:16:06 -0500

    add list item exercice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ImgCap from "./ImageCaption.js"
+var imageList = [
+  {id: 42, source: "http://placekitten.com/g/210/210", text: "Hello kittenz!"},
+  {id: 43, source: "https://facebook.github.io/react/img/logo.svg", text: "React Logo"},
+  {id: 44, source: "https://media.giphy.com/media/EldfH1VJdbrwY/giphy.gif", text: "Mind Blown!"}
+];
 
 class App extends Component {
   render() {
@@ -8,11 +14,20 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React test</h2>
+          <h2>Welcome to React Intro Workshop</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <h2>Testing Image Caption</h2>
+          <ImgCap source="http://i.imgur.com/D8JWn.jpg" text="Weird Cat!"/>
+          <hr/>
+        </div>
+        <div>
+          <h2>List of Items</h2>
+          {
+            imageList.map( (ele, i) => <ImgCap key={i} source={ele.source} alt={ele.text} text={ele.text}/> )
+          }
+          <hr/>
+        </div>
       </div>
     );
   }
