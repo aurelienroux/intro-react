d963a6df8610f2381327e154324dabf18cdbb33b
Aurelien Roux <aureroux13@me.com>
2017-02-15 10:42:08 -0500

    remove comments and add spaces

--- src/App.js
+++ src/App.js
@@ -17,23 +17,27 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React Intro Workshop</h2>
         </div>
-        <div>// Image Caption /////////////////////
+        
+        <div>
           <h2>Testing Image Caption</h2>
           <ImgCap source="http://i.imgur.com/D8JWn.jpg" text="Weird Cat!"/>
           <hr/>
         </div>
-        <div>// List of Items /////////////////////
+        
+        <div>
           <h2>List of Items</h2>
           {
             imageList.map( (ele, i) => <ImgCap key={i} source={ele.source} alt={ele.text} text={ele.text}/> )
           }
           <hr/>
         </div>
-        <div>// Layout /////////////////////
+
+        <div>
           <h2>Layout</h2>
           <Layout>
             <h3>About Us</h3>
@@ -41,26 +45,31 @@ class App extends Component {
           </Layout>
           <hr/>
         </div>
-        <div>// Guessing number /////////////////////
+
+        <div>
           <h2>Guess The Number Game</h2>
           <GuessTheNumber number={50} />
           <hr/>
         </div>
-        <div>// Clicking button /////////////////////
+
+        <div>
           <h2>You Clicked</h2>
           <ClickBtn />
           <hr/>
         </div>  
-        <div>// Character Counter /////////////////////
+        
+        <div>
           <h2>Character Counter</h2>
           <CharacCount />
           <hr/>
         </div>
-        <div>// Character limit /////////////////////
+
+        <div>
           <h2>Character Limit</h2>
           <CharacLim limit={30}/>
           <hr/>
         </div>
+        
       </div>
     );
   }
