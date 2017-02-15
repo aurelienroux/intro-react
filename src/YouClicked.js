62a8c999a6a4e5147483ca5f5f4fccf76a139fd2
Aurelien Roux <aureroux13@me.com>
2017-02-14 23:09:45 -0500

    add a conditional to hide reset message original

--- src/YouClicked.js
+++ src/YouClicked.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 class ClickBtn extends React.Component {
   state = {
-    num: "you never clicked"
+    num: "you never clicked",
+    reset: 0
   }
   _handleButtonClick = () => {
     var val = this.state.num
-    if( this.state.num === "you never clicked"){
+    if( val === "you never clicked"){
       this.setState({num: "you clicked once"})
-    } else if( this.state.num === "you clicked once"){
+    } else if( val === "you clicked once"){
       this.setState({num: 2})
     } else (
       this.setState({num: val + 1})
@@ -17,16 +18,28 @@ class ClickBtn extends React.Component {
 
   _reset = () => {
     this.setState({num: "you never clicked"})
+    this.setState({reset: this.state.reset + 1})
   }
 
   render(){
-    return(
-      <div>
-        <input type="button" value="Click Me!" onClick={this._handleButtonClick} />
-        <input type="button" value="Reset Me!" onClick={this._reset} />
-        <p>{this.state.num}</p>
-      </div>
-    )
+    if(this.state.reset === 0) {
+      return(
+        <div>
+          <input type="button" value="Click Me!" onClick={this._handleButtonClick} />
+          <input type="button" value="Reset Me!" onClick={this._reset} />
+          <p>{this.state.num}</p>
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <input type="button" value="Click Me!" onClick={this._handleButtonClick} />
+          <input type="button" value="Reset Me!" onClick={this._reset} />
+          <p>{this.state.num}</p>
+          <p>You reseted {this.state.reset} times</p>
+        </div>
+      )
+    }
   }
 }
 
