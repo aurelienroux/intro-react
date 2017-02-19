1f042d15ad8f6fdc3d3f50031020b88fe2556b02
Aurelien Roux <aureroux13@me.com>
2017-02-19 18:52:03 -0500

    improve image caption

--- src/ImageCaption.js
+++ src/ImageCaption.js
@@ -1,16 +1,33 @@
-import React from 'react';
+// import React from 'react';
+//
+// class ImageCaption extends React.Component {
+//   render() {
+//     return (
+//       <figure>
+//         <img src={this.props.source} alt={this.props.text} />
+//         <figcaption>
+//           {this.props.text}
+//         </figcaption>
+//       </figure>
+//     );
+//   }
+// }
+//
+// export default ImageCaption;
+//
 
-class ImageCaption extends React.Component {
-  render() {
+
+import React, { Component } from 'react';
+
+export default class ImageCaption extends Component{
+  render(){
     return (
       <figure>
         <img src={this.props.source} alt={this.props.text} />
         <figcaption>
-          {this.props.text}
+          { this.props.text }
         </figcaption>
       </figure>
-    );
+    )
   }
 }
-
-export default ImageCaption;
