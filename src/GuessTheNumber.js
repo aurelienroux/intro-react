31576290eb53a8b3ef2d9e60657925204dd7139e
Aurelien Roux <aureroux13@me.com>
2017-02-19 19:28:38 -0500

    upgraded guess number

--- src/GuessTheNumber.js
+++ src/GuessTheNumber.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
 class GuessTheNumber extends React.Component {
-  
-  handleButtonClick = () => {
+
+  handleButtonClick = (e) => {
+    e.preventDefault();
     if(this.props.number > this.refs.userGuess.value){
       alert("Too low")
     } else if (this.props.number < this.refs.userGuess.value){
@@ -10,14 +11,17 @@ class GuessTheNumber extends React.Component {
     } else {
       alert("You win!")
     }
+    this.refs.userGuess.value = "";
   }
 
   render(){
     return(
       <div>
         <p>Guess a number between 1 and 100</p>
-        <input type="number" ref="userGuess" />
-        <input type="button" value="GUESS" onClick={this.handleButtonClick} />
+        <form onSubmit= {this.handleButtonClick}>
+          <input type="number" ref="userGuess" />
+          <input type="button" value="GUESS" />
+        </form>
       </div>
     )
   }
